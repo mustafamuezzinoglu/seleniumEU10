aa3253e76e7befcb3ade8db741b243d5ceb2da24
Mustafa <mustafamuezzinoglu@gmail.com>
2023-01-10 18:22:09 +0300

    day6 completed

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/Day6_Tasks.java
@@ -0,0 +1,135 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.ChromeStartUtility;
+import com.cydeo.utilities.WaitUtility;
+import com.google.common.base.Verify;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Day6_Tasks {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        //1. Open Chrome browser
+        driver = ChromeStartUtility.chromeStart();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//      2.  Go to website: http://practice.cydeo.com/javascript_alerts
+        driver.get("http://practice.cybertekschool.com/javascript_alerts");
+    }
+
+//    TC #1: Information alert practice
+
+    @Test
+    public void Task1_JsAlert(){
+
+//    3. Click to “Click for JS Alert” button
+        WebElement jsAlertButton = driver.findElement(By.xpath("//button[@onclick='jsAlert()']"));
+        jsAlertButton.click();
+
+//    To be able to click to Alert OK button we need to switch driver's focus to Alert itself.
+        Alert alert = driver.switchTo().alert();
+
+//4. Click to OK button from the alert
+        WaitUtility.waitSeconds(1);
+        alert.accept();
+
+//5. Verify “You successfully clicked an alert” text is displayed.
+        WebElement afterAlertText = driver.findElement(By.cssSelector("p#result"));
+
+        //Failure message will only be displayed if assertion fails: "Result text is NOT displayed."
+        Assert.assertTrue(afterAlertText.isDisplayed(), "result text is Not displayed");
+
+        System.out.println("You successfully clicked an alert is Displayed = " + afterAlertText.isDisplayed());
+
+        String expectedText = "You successfully clicked an alert";
+        String actualText = afterAlertText.getText();
+
+        Assert.assertEquals(actualText,expectedText, "Actual result text is not as expected!!!");
+
+    }
+
+    @Test
+    public void Task2_JsConfirm(){
+//        TC #2: Confirmation alert practice
+//        1. Open browser
+//        2. Go to website: http://practice.cydeo.com/javascript_alerts
+//        3. Click to “Click for JS Confirm” button
+        WebElement jsConfirmButton = driver.findElement(By.xpath("//button[@onclick='jsConfirm()']"));
+        jsConfirmButton.click();
+
+//    To be able to click to Alert OK button we need to switch driver's focus to Alert itself.
+        Alert alert = driver.switchTo().alert();
+
+//4. Click to OK button from the alert
+        WaitUtility.waitSeconds(1);
+        alert.accept();
+
+//        5. Verify “You clicked: Ok” text is displayed.
+        WebElement afterAlertText = driver.findElement(By.cssSelector("p#result"));
+
+        //Failure message will only be displayed if assertion fails: "Result text is NOT displayed."
+        Assert.assertTrue(afterAlertText.isDisplayed(), "result text is Not displayed");
+
+        System.out.println("You clicked: Ok is Displayed = " + afterAlertText.isDisplayed());
+
+        String expectedText = "You clicked: Ok";
+        String actualText = afterAlertText.getText();
+
+        Assert.assertEquals(actualText,expectedText, "Actual result text is not as expected!!!");
+
+    }
+
+    @Test
+    public void Task2_JsPrompt(){
+//    TC #3: Information alert practice
+//        1. Open browser
+//        2. Go to website: http://practice.cydeo.com/javascript_alerts
+//        3. Click to “Click for JS Prompt” button
+        WebElement jsPromptButton = driver.findElement(By.xpath("//button[@onclick='jsPrompt()']"));
+        jsPromptButton.click();
+
+//    To be able to click to Alert OK button we need to switch driver's focus to Alert itself.
+        Alert alert = driver.switchTo().alert();
+
+//4. Click to OK button from the alert
+        WaitUtility.waitSeconds(2);
+        alert.sendKeys("hello");
+
+        WaitUtility.waitSeconds(3);
+        alert.accept();
+
+//        5. Verify “You clicked: Ok” text is displayed.
+        WebElement afterAlertText = driver.findElement(By.cssSelector("p#result"));
+
+        //Failure message will only be displayed if assertion fails: "Result text is NOT displayed."
+        Assert.assertTrue(afterAlertText.isDisplayed(), "result text is Not displayed");
+
+        System.out.println("You entered: hello is Displayed = " + afterAlertText.isDisplayed());
+
+        String expectedText = "You entered: hello";
+        String actualText = afterAlertText.getText();
+
+        Assert.assertEquals(actualText,expectedText, "Actual result text is not as expected!!!");
+
+    }
+
+
+//1. Open browser
+//2. Go to website: http://practice.cydeo.com/javascript_alerts
+//            3. Click to “Click for JS Prompt” button
+//4. Send “hello” text to alert
+//5. Click to OK button from the alert
+//6. Verify “You entered: hello” text is displayed.
+}
---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_Iframe_Practice.java
@@ -0,0 +1,66 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.ChromeStartUtility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_Iframe_Practice {
+
+    //    TC #4: Iframe practice
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+//1. Open Chrome browser
+        driver = ChromeStartUtility.chromeStart();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//      2.  Go to website: http://practice.cydeo.com/javascript_alerts
+        driver.get("http://practice.cybertekschool.com/iframe");
+    }
+
+//    @Ignore if we want to pass the test not running the test  we should to do @ignore
+    @Test
+    public void iframe_test() {
+
+//        We need to switch driver's focus to iframe
+
+//        option#1 - switching to iframe using id attribute value
+//        driver.switchTo().frame("mce_0_ifr");
+
+//        option#2 - passing index number of iframe
+//        driver.switchTo().frame(0);
+
+//        option#2 - passing index number of iframe
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
+
+//        to locate the p tag
+        WebElement yourContentGoesThere = driver.findElement(By.xpath("//p"));
+
+//  4. Assert: “Your content goes here.” Text is displayed.
+
+        Assert.assertTrue(yourContentGoesThere.isDisplayed());
+
+        //Verify "An iFrame containing the TinyMCE WYSIWYG Editor"
+        //To be able to verify the header, we must switch back to "main HTML"
+        driver.switchTo().parentFrame();
+
+//  5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor”
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+
+//      assertion of header text is displayed or not
+        Assert.assertTrue(headerText.isDisplayed());
+
+
+    }
+
+
+}
