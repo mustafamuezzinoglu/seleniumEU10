c23f9f7722848129588a30cac6ee89f0751ffd71
Mustafa <mustafamuezzinoglu@gmail.com>
2023-02-19 23:03:13 +0300

    Update TestBase.java
    add * after annotation.

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -2,8 +2,7 @@ package com.cydeo.tests.base;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
