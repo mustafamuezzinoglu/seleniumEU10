fe3675976eb231b5b6ce13422ced2f0b7eeeb2b0
Mustafa <mustafamuezzinoglu@gmail.com>
2023-02-19 23:11:10 +0300

    Update TestBase.java
    add *

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -1,11 +1,8 @@
 package com.cydeo.tests.base;
 
-import com.cydeo.utilities.ConfigurationReader;
-import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.*;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.*;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
